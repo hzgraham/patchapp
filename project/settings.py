42e3216fb76ce2f9d0097d1dae95315c6c1af549
hgraham <hgraham@redhat.com>
2015-07-27 23:17:07 -0400

    adding javascript and css stuff and changing the menubar to a js navbar

--- project/settings.py
+++ project/settings.py
@@ -114,7 +114,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-# STATICFILES_DIRS =( os.path.join(STATIC_ROOT, 'css/'),
-#                     os.path.join(STATIC_ROOT, 'javascript/'),
-#                     os.path.join(STATIC_ROOT, 'images/')
-# )
+STATICFILES_DIRS =( os.path.join(STATIC_ROOT, 'css/'),
+                    os.path.join(STATIC_ROOT, 'javascript/'),
+                    os.path.join(STATIC_ROOT, 'images/'),
+                    os.path.join(STATIC_ROOT, 'autopatch/')
+)
