cfd9c917b00cf68e0a8de8dd51ef70ce9813fc62
hgraham <hgraham@redhat.com>
2015-07-27 22:06:43 -0400

    changing location of static files and adding folders for css javascript and images to autopatch app

--- project/settings.py
+++ project/settings.py
@@ -113,8 +113,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-# STATICFILES_DIRS =( os.path.join(STATIC_ROOT, 'css/'),
-#                     os.path.join(STATIC_ROOT, 'javascript/'),
-#                     os.path.join(STATIC_ROOT, 'images/')
-# )
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS =( os.path.join(STATIC_ROOT, 'css/'),
+                    os.path.join(STATIC_ROOT, 'javascript/'),
+                    os.path.join(STATIC_ROOT, 'images/')
+)
