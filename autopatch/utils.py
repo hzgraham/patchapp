6da8bb7ffcf9e279c82608b64d2a58d0716fc714
hgraham <hgraham@redhat.com>
2015-08-02 20:48:13 -0400

    use git to import data now works for issue #1

--- autopatch/utils.py
+++ autopatch/utils.py
@@ -1,11 +1,52 @@
-import urllib.request, urllib.error, git
+import urllib.request, urllib.error, git, shutil, os, glob
 from django.http import Http404
 from .models import Server,Hosttotal
 
 class ModMaint():
     def parseGit(self, manifests):
-        repo = git.Repo.clone_from(manifests,'autopatch/manifests')
-        print("this is the repo: ",repo)
+        git_path = 'autopatch/manifests'
+        if not os.path.isdir(git_path):
+            repo = git.Repo.clone_from(manifests,'autopatch/manifests')
+            print("this is the repo: ",repo)
+        else:
+            #shutil.rmtree(git_path)
+            print("The path already exists",git_path)
+        host_paths = []
+        host_paths.extend(glob.glob(git_path+'/nodes/*'))
+        print("Host Paths",host_paths)
+        for each in host_paths:
+            mgmt = ''
+            hostgroup = ''
+            exclude = ''
+            skip = ''
+            pathname = each+'/maint.yaml'
+            if os.path.exists(pathname):
+                with open(pathname, 'rt') as myfile:
+                    lines = myfile.readlines()
+                    for i in lines:
+                        if 'syspatch_mgmt: IT-Platops' == i.split("\n")[0]:
+                            mgmt = i.split(":")[1].strip().split("\n")[0]
+                        if 'syspatch_hostgroup' == i.split(":")[0]:
+                            hostgroup = i.split(":")[1].strip().split("\n")[0]
+                        if 'syspatch_yum_excludes' == i.split(":")[0]:
+                            exclude = i.split(":")[1].strip().split("\n")[0]
+                        if 'syspatch_skip' == i.split(":")[0]:
+                            skip = i.split(":")[1].strip().split("\n")[0]
+                            # if i.split(":")[1].strip().split("\n")[0] is '1':
+                            #     skip = 'TRUE'
+                            # elif i.split(":")[1].strip().split("\n")[0] is '0':
+                            #     z = 'FALSE
+                    servername = each.split('/')[-1]
+                    print("servername: ",servername)
+                    s = Server(server=servername)
+                    s.server = each.split('/')[-1]
+                    s.mgmt = mgmt
+                    s.exclude = exclude
+                    s.skip = skip
+                    s.hostgroup = hostgroup
+                    print("server: ",s.server)
+                    s.save()
+                myfile.close()
 
     def getMaint(self, url):
         syspatch = {}
