ed596478829470d05616bc019a6dd7a1a0183701
Andrew Butcher <abutcher@redhat.com>
2015-08-13 15:18:21 -0400

    Remove welcome namespace and redirect / -> /autopatch/

--- project/settings.py
+++ project/settings.py
@@ -44,7 +44,6 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'debug_toolbar',
-    'welcome',
     'autopatch',
 )
 
--- project/urls.py
+++ project/urls.py
@@ -1,15 +1,11 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.views.generic import RedirectView
 
 admin.autodiscover()
 
 urlpatterns = [
-    # Examples:
-    # url(r'^$', 'project.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-    
-    url(r'^$', 'welcome.views.index'),
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', RedirectView.as_view(url='/autopatch/', permanent=True)),
     url(r'^autopatch/', include('autopatch.urls', namespace="autopatch")),
-
+    url(r'^admin/', include(admin.site.urls)),
 ]
