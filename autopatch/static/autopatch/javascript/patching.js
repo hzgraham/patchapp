13cf347f31933dec034143fa4c8ab3ee828d4125
Andrew Butcher <abutcher@redhat.com>
2015-08-15 15:50:10 -0400

    Indicate sort direction with icons. Replace jquery with latest compressed.

--- autopatch/static/autopatch/javascript/patching.js
+++ autopatch/static/autopatch/javascript/patching.js
@@ -1,9 +1,10 @@
-var $rows = $('#list-servers tr');
-$('#input-server-list').keyup(function() {
-    var val = $.trim($(this).val()).replace(/ +/g, ' ').toLowerCase();
-
-    $rows.show().filter(function() {
-	var text = $(this).text().replace(/\s+/g, ' ').toLowerCase();
-	return !~text.indexOf(val);
-    }).hide();
+$(document).ready(function(){
+    var $rows = $('#list-servers tr');
+    $('#input-server-list').keyup(function() {
+	var val = $.trim($(this).val()).replace(/ +/g, ' ').toLowerCase();
+	$rows.show().filter(function() {
+	    var text = $(this).text().replace(/\s+/g, ' ').toLowerCase();
+	    return !~text.indexOf(val);
+	}).hide();
+    });
 });
