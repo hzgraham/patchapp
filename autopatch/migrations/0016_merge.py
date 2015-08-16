b843d0a943dcdae86d97f705cebc09f05b527701
hgraham <hgraham@redhat.com>
2015-08-16 16:48:36 -0400

    merged a migration conflict

---
+++ autopatch/migrations/0016_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('autopatch', '0015_auto_20150816_2002'),
+        ('autopatch', '0015_auto_20150816_1837'),
+    ]
+
+    operations = [
+    ]
