e0b2757647281d2074f5ddd7e6305bb9ccb123fe
hgraham <hgraham@redhat.com>
2015-08-30 15:01:42 -0400

    closes #21 - removing the GetList and getMaint functions because you don't want to pull git data from https when you can clone

--- autopatch/utils.py
+++ autopatch/utils.py
@@ -281,34 +281,6 @@ class ModMaint():
             env = "unassigned"
         return env
 
-    def getMaint(self, url):
-        syspatch = {}
-        lines = []
-        try:
-            myurl = urllib.request.urlopen(url)
-            lines = myurl.readlines()
-        except urllib.error.HTTPError as e:
-            # raise Http404("Poll does not exist")
-            pass
-        mgmt = ""
-        hostgroup = ""
-        exclude = ""
-        skip = ""
-        for i in lines:
-            if b'syspatch_mgmt: IT-Platops' == i.split(b"\n")[0]:
-                mgmt = i.split(b":")[1].strip().split(b"\n")[0]
-            if b'syspatch_hostgroup' == i.split(b":")[0]:
-                hostgroup = i.split(b":")[1].strip().split(b"\n")[0]
-            if b'syspatch_yum_excludes' == i.split(b":")[0]:
-                exclude = i.split(b":")[1].strip().split(b"\n")[0]
-            if b'syspatch_skip' == i.split(b":")[0]:
-                skip = i.split(b":")[1].strip().split(b"\n")[0]
-            syspatch = {'mgmt': mgmt, 'hostgroup': hostgroup, 'exclude': exclude, 'skip': skip}
-            # print(syspatch)
-            # if syspatch is not dict:
-            # syspatch = {}
-        return syspatch
-
     def genCSV(self, servers):
         s = []
         params = [['Hostname','Excluded packages','Skip','Hostgroup','Comments','Pastebin Link (If Errors Are Present)']]
